3e1e79966a19b9c54a0ae71321383144b988ff3a
bowmessage <bowmessage@gmail.com>
2014-11-18 01:01:23 -0600

    Added camera button: requests user to take photo.

--- android/src/edu/tamu/csce462/etchasketcher/MainActivity.java
+++ android/src/edu/tamu/csce462/etchasketcher/MainActivity.java
@@ -1,15 +1,35 @@
 package edu.tamu.csce462.etchasketcher;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+	
+	
+	static final int REQUEST_IMAGE_CAPTURE = 1;
+	private void dispatchTakePictureIntent() {
+	    Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+	    if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+	        startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+	    }
+	}
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        Button takePictureButton = (Button) findViewById(R.id.takePictureButton);
+		takePictureButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	dispatchTakePictureIntent();
+            }
+        });
     }
 
 
