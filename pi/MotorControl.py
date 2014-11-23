6ec4dfa8c52fb3021e96a209151c1434d942ba62
James Bowman <bowmessage@gmail.com>
2014-11-22 20:06:38 -0600

    Barebones motor stepping, cw and ccw.

--- pi/MotorControl.py
+++ pi/MotorControl.py
@@ -1,3 +1,37 @@
-#testing pi autosync on boot.
-print "Test"
-print "Another test."
+import RPi.GPIO as GPIO, time, os
+
+GPIO.setmode(GPIO.BCM)
+
+delayTime = .007
+
+def setup():
+  GPIO.setup(23, GPIO.OUT) #dir
+  GPIO.setup(24, GPIO.OUT) #step probe
+  GPIO.setup(25, GPIO.OUT) #always high
+
+def run():
+  GPIO.output(25, GPIO.HIGH)
+  while True:
+    cycle()
+
+def cycle():
+  for x in range(0,1000):
+    cwStep()
+
+def ccwStep():
+  GPIO.output(23, GPIO.LOW)
+  step()
+
+def cwStep():
+  GPIO.output(23, GPIO.HIGH)
+  step()
+  
+def step():
+  GPIO.output(24, GPIO.HIGH)
+  time.sleep(delayTime)
+  GPIO.output(24, GPIO.LOW)
+  time.sleep(delayTime)
+
+setup()
+run()
+GPIO.cleanup()
