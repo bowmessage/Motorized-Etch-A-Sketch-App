68ff1c2589c82959a379b8932c232f8a724010fa
James Bowman <jamesbowman@tamu.edu>
2014-12-06 21:01:30 -0600

    Consolidate buttons to Spinner, create circle draw button.

--- android/src/edu/tamu/csce462/etchasketcher/MainActivity.java
+++ android/src/edu/tamu/csce462/etchasketcher/MainActivity.java
@@ -28,8 +28,10 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
@@ -170,11 +172,36 @@ public class MainActivity extends Activity {
 		}
 
 	}
+	
+	protected String pointsStringForCircleOfRadius(int radius){
+		StringBuilder ret = new StringBuilder();
+		ret.append("[");
+		for(float i = 0; i < 2*Math.PI; i += Math.PI/100){
+			String x = "" + (double) Math.round(Math.cos(i) * radius * 100000) / 100000;
+			String y = "" + (double) Math.round(Math.sin(i) * radius * 100000) / 100000;
+			
+			ret.append(x + "," + y + ",");
+		}
+		ret.deleteCharAt(ret.length()-1);
+		ret.append("]");
+		return ret.toString();
+	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		Spinner spinner = (Spinner) findViewById(R.id.spinner1);
+		// Create an ArrayAdapter using the string array and a default spinner
+		// layout
+		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+				this, R.array.drawable_array,
+				android.R.layout.simple_spinner_item);
+		// Specify the layout to use when the list of choices appears
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		// Apply the adapter to the spinner
+		spinner.setAdapter(adapter);
 
 		// generate various buttons...
 		Button takePictureButton = (Button) findViewById(R.id.takePictureButton);
@@ -187,128 +214,34 @@ public class MainActivity extends Activity {
 			}
 		});
 
-		Button drawCircleButton = (Button) findViewById(R.id.drawCircleButton);
-		drawCircleButton.setOnClickListener(new OnClickListener() {
+		Button drawButton = (Button) findViewById(R.id.drawButton);
+		drawButton.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				if (isConnected == true) {
-					PrintWriter writer = new PrintWriter(outputStream);
-					writer.write("circle test");
-					writer.flush();
-				} else {
-					Context context = getApplicationContext();
-					CharSequence text = "Must connect to device first!";
-					int duration = Toast.LENGTH_SHORT;
+				if (true) {
+					//PrintWriter writer = new PrintWriter(outputStream);
+					Spinner spinner = (Spinner) findViewById(R.id.spinner1);
+					String selected = (String) spinner.getSelectedItem();
 
-					Toast toast = Toast.makeText(context, text, duration);
-					toast.show();
-				}
-			}
-		});
-
-		Button drawSquareButton = (Button) findViewById(R.id.drawSquareButton);
-		drawSquareButton.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				if (isConnected == true) {
-					PrintWriter writer = new PrintWriter(outputStream);
-					writer.write("[100.0,100.0,100.0,500.0,500.0,500.0,500.0,100.0,100.0,100.0]");
-					writer.flush();
-				} else {
-					Context context = getApplicationContext();
-					CharSequence text = "Must connect to device first!";
-					int duration = Toast.LENGTH_SHORT;
-
-					Toast toast = Toast.makeText(context, text, duration);
-					toast.show();
-				}
-			}
-		});
-
-		Button drawTriangleButton = (Button) findViewById(R.id.drawTriangleButton);
-		drawTriangleButton.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				if (isConnected == true) {
-					PrintWriter writer = new PrintWriter(outputStream);
-					writer.write("[500,100,50,500,950,500,500,100");
-					writer.flush();
-				} else {
-					Context context = getApplicationContext();
-					CharSequence text = "Must connect to device first!";
-					int duration = Toast.LENGTH_SHORT;
-
-					Toast toast = Toast.makeText(context, text, duration);
-					toast.show();
-				}
-			}
-		});
-
-		Button drawTAMUButton = (Button) findViewById(R.id.drawTAMUButton);
-		drawTAMUButton.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				if (isConnected == true) {
-					PrintWriter writer = new PrintWriter(outputStream);
-					writer.write("[200,100,200,150,300,150,300,125,500,125,500,500,300,500,300,550,750,550,750,500,700,500,700,125,900,125,900,150,950,150,950,100,200,100]");
-					writer.flush();
-				} else {
-					Context context = getApplicationContext();
-					CharSequence text = "Must connect to device first!";
-					int duration = Toast.LENGTH_SHORT;
-
-					Toast toast = Toast.makeText(context, text, duration);
-					toast.show();
-				}
-			}
-		});
-
-		Button drawPuppyButton = (Button) findViewById(R.id.drawPuppyButton);
-		drawPuppyButton.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				if (isConnected == true) {
-					PrintWriter writer = new PrintWriter(outputStream);
-					writer.write("puppy test");
-					writer.flush();
-				} else {
-					Context context = getApplicationContext();
-					CharSequence text = "Must connect to device first!";
-					int duration = Toast.LENGTH_SHORT;
-
-					Toast toast = Toast.makeText(context, text, duration);
-					toast.show();
-				}
-			}
-		});
-
-		Button drawInfoButton = (Button) findViewById(R.id.drawInfoButton);
-		drawInfoButton.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				if (isConnected == true) {
-					PrintWriter writer = new PrintWriter(outputStream);
-					writer.write("info test");
-					writer.flush();
-				} else {
-					Context context = getApplicationContext();
-					CharSequence text = "Must connect to device first!";
-					int duration = Toast.LENGTH_SHORT;
-
-					Toast toast = Toast.makeText(context, text, duration);
-					toast.show();
-				}
-			}
-		});
-
-		Button frameButton = (Button) findViewById(R.id.frameButton);
-		frameButton.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				if (isConnected == true) {
-					PrintWriter writer = new PrintWriter(outputStream);
-					writer.write("[0,0,0,650,1000,650,1000,0,0,0]");
-					writer.flush();
+					String toWrite = "";
+					if (selected.equals("Circle")) {
+						toWrite = pointsStringForCircleOfRadius(100);
+					} else if (selected.equals("Square")) {
+						toWrite = "[100.0,100.0,100.0,500.0,500.0,500.0,500.0,100.0,100.0,100.0]";
+					} else if (selected.equals("Triangle")) {
+						toWrite = "[500,100,50,500,950,500,500,100";
+					} else if (selected.equals("Puppy")) {
+						toWrite = "puppy test";
+					} else if (selected.equals("TAMU")) {
+						toWrite = "[200,100,200,150,300,150,300,125,500,125,500,500,300,500,300,550,750,550,750,500,700,500,700,125,900,125,900,150,950,150,950,100,200,100]";
+					} else if (selected.equals("Info")) {
+						toWrite = "info test";
+					} else if (selected.equals("Frame")) {
+						toWrite = "[0,0,0,650,1000,650,1000,0,0,0]";
+					}
+					Log.d("", toWrite);
+					//writer.write(toWrite);
+					//writer.flush();
 				} else {
 					Context context = getApplicationContext();
 					CharSequence text = "Must connect to device first!";
@@ -448,6 +381,8 @@ public class MainActivity extends Activity {
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
+
+
 		return true;
 	}
 
